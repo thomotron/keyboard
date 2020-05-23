59e93e92fd193b92059cf3d1f10a563a57b48f2f
Thomas Wade <gigatech55@gmail.com>
2020-05-23 23:11:28 +0930

    Add keyboard map and handler

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -8,7 +8,27 @@
 #define DATA_LED A, 1
 #define BUTTON D, 6
 
+void readMatrix();
+
 PS2dev ps2;
+
+// A single key on the keyboard.
+typedef struct key {
+    unsigned char code;
+    bool special;
+    bool lastPressedState;
+} key;
+
+// Table containing character codes for each key in the keyboard layout
+// This firmware was designed for a 19x6 matrix (114 keys total).
+key kbmap[6][19] = {
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}},
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}},
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}},
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}},
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}},
+    {{0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}, {0x00, false, false}}
+};
 
 void init()
 {
@@ -46,4 +66,51 @@ int main()
             ps2.keyboard_mkbrk(ps2.ENTER);
         }
     }
+}
+
+// Polls each key in the keyboard matrix and processes any changes.
+// This includes both setting their last state in kbmap and sending make/break codes to the host.
+void readMatrix()
+{
+    for (int row = 0; row < 6; row++)
+    {
+        // Set the row high and others low
+        PORTD = 0b11111111 & ~(1 << row);
+
+        // Wait one cycle for the MCU to do its thing
+        asm volatile ("nop");
+
+        // Pack signal lines A-S into a 32-bit integer (19 > 16, so 32 is the next in line)
+        uint32_t columnData = (PINA << 11) | (PINB << 3) | (PINC >> 5);
+
+        for (int col = 18; col >= 0; col--)
+        {
+            // Get the key in question from the map
+            key* k = &kbmap[row][col];
+
+            // Get the state of the key from the matrix
+            bool newPressedState = ((columnData >> col) & 0b1) == High;
+
+            if (k->lastPressedState != newPressedState)
+            {
+                // Send the new key state to the host
+                if (k->special)
+                {
+                    // Special key, use the special make/break prefix
+                    newPressedState ? ps2.keyboard_press_special(k->code) : ps2.keyboard_release_special(k->code);
+                }
+                else
+                {
+                    // Normal key, use the normal make/break prefix
+                    newPressedState ? ps2.keyboard_press(k->code) : ps2.keyboard_release(k->code);
+                }
+
+                // Set the new state
+                k->lastPressedState = newPressedState;
+            }
+        }
+    }
+
+    // Set PORTD low
+    PORTD = 0b11111111;
 }
