ef2a130661a709497923f24b7043c8f898a7d531
Thomas Wade <gigatech55@gmail.com>
2020-11-22 11:04:38 +1030

    Add debounce logic
    Key changes are ignored for one cycle to allow them to settle.

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -43,6 +43,7 @@ typedef struct key {
     unsigned char code;
     bool special;
     bool lastPressedState;
+    bool isBouncing;
 } key;
 
 /// Status LEDs states (i.e. numlock, capslock, etc.)
@@ -65,12 +66,12 @@ bool fnPressed = false;
 
 /// Table containing character codes for each key in the keyboard layout.
 key kbmap[MATRIX_HEIGHT][MATRIX_WIDTH] = {
-    {{0x76, false, false},{0x05, false, false},{0x06, false, false},{0x04, false, false},{0x0C, false, false},{0x03, false, false},{0x0B, false, false},{0x83, false, false},{0x0A, false, false},{0x01, false, false},{0x09, false, false},{0x78, false, false},{0x07, false, false},{0x70, true, false},{0x6C, true, false},{0x7D, true, false},{0x77, false, false},{0x00, false, false},{0x00, false, false}},
-    {{0x0E, false, false},{0x16, false, false},{0x1E, false, false},{0x26, false, false},{0x25, false, false},{0x2E, false, false},{0x36, false, false},{0x3D, false, false},{0x3E, false, false},{0x46, false, false},{0x45, false, false},{0x4E, false, false},{0x55, false, false},{0x66, false, false},{0x71, true, false},{0x69, true, false},{0x7A, true, false},{0x00, false, false},{0x00, false, false}},
-    {{0x0D, false, false},{0x15, false, false},{0x1D, false, false},{0x24, false, false},{0x2D, false, false},{0x2C, false, false},{0x35, false, false},{0x3C, false, false},{0x43, false, false},{0x44, false, false},{0x4D, false, false},{0x54, false, false},{0x5B, false, false},{0x5D, false, false},{0x6C, false, false},{0x4A, true, false},{0x7C, false, false},{0x00, false, false},{0x00, false, false}},
-    {{0x58, false, false},{0x1C, false, false},{0x1B, false, false},{0x23, false, false},{0x2B, false, false},{0x34, false, false},{0x33, false, false},{0x3B, false, false},{0x42, false, false},{0x4B, false, false},{0x4C, false, false},{0x52, false, false},{0x5A, false, false},{0x6B, false, false},{0x75, false, false},{0x7D, false, false},{0x7B, false, false},{0x00, false, false},{0x00, false, false}},
-    {{0x12, false, false},{0x1A, false, false},{0x22, false, false},{0x21, false, false},{0x2A, false, false},{0x32, false, false},{0x31, false, false},{0x3A, false, false},{0x41, false, false},{0x3B, true, false},{0x4A, false, false},{0x59, false, false},{0x75, true, false},{0x69, false, false},{0x73, false, false},{0x74, false, false},{0x79, false, false},{0x00, false, false},{0x00, false, false}},
-    {{0x14, false, false},{0x1F, true, false},{0x11, false, false},{0x00, false, false},{0x29, false, false},{0x00, false, false},{0x11, true, false},{0x00, false, false},{0x14, true, false},{0x6B, true, false},{0x72, true, false},{0x74, true, false},{0x70, false, false},{0x72, false, false},{0x7A, false, false},{0x71, false, false},{0x5A, true, false},{0x00, false, false},{0x00, false, false}}
+    {{0x76, false, false, false},{0x05, false, false, false},{0x06, false, false, false},{0x04, false, false, false},{0x0C, false, false, false},{0x03, false, false, false},{0x0B, false, false, false},{0x83, false, false, false},{0x0A, false, false, false},{0x01, false, false, false},{0x09, false, false, false},{0x78, false, false, false},{0x07, false, false, false},{0x70, true, false, false},{0x6C, true, false, false},{0x7D, true, false, false},{0x77, false, false, false},{0x00, false, false, false},{0x00, false, false, false}},
+    {{0x0E, false, false, false},{0x16, false, false, false},{0x1E, false, false, false},{0x26, false, false, false},{0x25, false, false, false},{0x2E, false, false, false},{0x36, false, false, false},{0x3D, false, false, false},{0x3E, false, false, false},{0x46, false, false, false},{0x45, false, false, false},{0x4E, false, false, false},{0x55, false, false, false},{0x66, false, false, false},{0x71, true, false, false},{0x69, true, false, false},{0x7A, true, false, false},{0x00, false, false, false},{0x00, false, false, false}},
+    {{0x0D, false, false, false},{0x15, false, false, false},{0x1D, false, false, false},{0x24, false, false, false},{0x2D, false, false, false},{0x2C, false, false, false},{0x35, false, false, false},{0x3C, false, false, false},{0x43, false, false, false},{0x44, false, false, false},{0x4D, false, false, false},{0x54, false, false, false},{0x5B, false, false, false},{0x5D, false, false, false},{0x6C, false, false, false},{0x4A, true, false, false},{0x7C, false, false, false},{0x00, false, false, false},{0x00, false, false, false}},
+    {{0x58, false, false, false},{0x1C, false, false, false},{0x1B, false, false, false},{0x23, false, false, false},{0x2B, false, false, false},{0x34, false, false, false},{0x33, false, false, false},{0x3B, false, false, false},{0x42, false, false, false},{0x4B, false, false, false},{0x4C, false, false, false},{0x52, false, false, false},{0x5A, false, false, false},{0x6B, false, false, false},{0x75, false, false, false},{0x7D, false, false, false},{0x7B, false, false, false},{0x00, false, false, false},{0x00, false, false, false}},
+    {{0x12, false, false, false},{0x1A, false, false, false},{0x22, false, false, false},{0x21, false, false, false},{0x2A, false, false, false},{0x32, false, false, false},{0x31, false, false, false},{0x3A, false, false, false},{0x41, false, false, false},{0x3B, true, false, false},{0x4A, false, false, false},{0x59, false, false, false},{0x75, true, false, false},{0x69, false, false, false},{0x73, false, false, false},{0x74, false, false, false},{0x79, false, false, false},{0x00, false, false, false},{0x00, false, false, false}},
+    {{0x14, false, false, false},{0x1F, true, false, false},{0x11, false, false, false},{0x00, false, false, false},{0x29, false, false, false},{0x00, false, false, false},{0x11, true, false, false},{0x00, false, false, false},{0x14, true, false, false},{0x6B, true, false, false},{0x72, true, false, false},{0x74, true, false, false},{0x70, false, false, false},{0x72, false, false, false},{0x7A, false, false, false},{0x71, false, false, false},{0x5A, true, false, false},{0x00, false, false, false},{0x00, false, false, false}}
 };
 
 /// Timer0 matching comparison interrupt
@@ -198,9 +199,21 @@ inline void readMatrix()
 /// updated and the corresponding make/break code is sent to the host.
 void handleKeypress(key* key, bool value)
 {
-    // Only update the key if the state has changed
+    // Make sure any key state changes are debounced for one cycle
     if (key->lastPressedState != value)
     {
+        // Update the state and ignore it
+        key->lastPressedState = value;
+        key->isBouncing = true;
+        return;
+    }
+
+    // Has the key previously been ignored and is still the same value?
+    // i.e. is it stable?
+    if (key->isBouncing && key->lastPressedState == value)
+    {
+        // Reset the debounce flag and handle the keypress normally
+        key->isBouncing = false;
         key->lastPressedState = value;
         if (value == High)
         {
