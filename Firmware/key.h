5518cb394c1647fe9ec2202824cef49bb6b2dabd
Thomas Wade <tom@tem.party>
2021-12-01 18:53:57 +1030

    Consense bool fields to conserve flash

--- Firmware/key.h
+++ Firmware/key.h
@@ -6,9 +6,9 @@
 // A single key on the keyboard.
 typedef struct key {
     uint8_t code;
-    bool special;
-    bool lastPressedState;
-    bool isBouncing;
+    bool special : 1;
+    bool lastPressedState : 1;
+    bool isBouncing : 1;
     uint16_t lastChange = 0;
 } key;
 
